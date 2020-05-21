d1e179e0fb73f7d4cb4fb9a02294b5799687d88a
Kavini <kavininanayakkara@gmail.com>
2020-05-21 19:33:49 +0530

    Life cycle call back methods

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -1,12 +1,13 @@
-package com.example.demoapp;
+package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
 import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
